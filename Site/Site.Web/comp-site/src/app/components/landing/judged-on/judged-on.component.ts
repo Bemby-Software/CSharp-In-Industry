fee94a371dffa15118968a352a9a90c40a7e2909
William Mumby <williammumby@Williams-MacBook-Pro.local>
2021-04-11 19:24:10 +0100

    basic layout of the page has been done, kind of picked the colour scheme

--- Site/Site.Web/comp-site/src/app/components/landing/judged-on/judged-on.component.ts
+++ Site/Site.Web/comp-site/src/app/components/landing/judged-on/judged-on.component.ts
@@ -1,4 +1,7 @@
  import { Component, OnInit } from '@angular/core';
+import { ICriteria } from 'src/app/models/criteria';
+
+const dummyDescription: string = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit. sit, amet consectetur adipisicing elit. Lorem ipsum dolor sit, amet consectetur adipisicing elit.";
 
 @Component({
   selector: 'app-judged-on',
@@ -7,11 +10,46 @@
 })
 export class JudgedOnComponent implements OnInit {
 
-  list: number[] = [1,2,3,4,5,6];
+  criteria: ICriteria[] = [
+    {
+      title: "Readability",
+      icon: "glasses",
+      description: dummyDescription
+    },
+    {
+      title: "Structure",
+      icon: "warehouse",
+      description: dummyDescription
+    },
+    {
+      title: "Quality",
+      icon: "clipboard-check",
+      description: dummyDescription
+    },
+    {
+      title: "Testing",
+      icon: "vial",
+      description: dummyDescription
+    },
+    {
+      title: "Time Management",
+      icon: "clock",
+      description: dummyDescription
+    },
+    {
+      title: "Process",
+      icon: "project-diagram",
+      description: dummyDescription
+    }
+  ]
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  getClassForCriteriaIcon(i: ICriteria) {
+    return `fas text-8xl fa-${i.icon}`;
   }
 
 }
---
+++ Site/Site.Web/comp-site/src/app/models/criteria.ts
@@ -0,0 +1,5 @@
+export interface ICriteria {
+    icon: string;
+    title: string;
+    description: string;
+}
