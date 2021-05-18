2bc03a755efdf183b040fe82c66e37af1071efc8
Billy Mumby <billy.mumby@outlook.com>
2021-05-18 08:58:00 +0100

    added directory searcher

---
+++ Site/tests/Site.Testing.Common/Helpers/DirectorySearcher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace Site.Testing.Common.Helpers
+{
+    public static class DirectorySearcher
+    {
+        public static string SearchForFullPath(string path, string suffix)
+        {
+            var current = new DirectoryInfo(Directory.GetCurrentDirectory());
+            var fullPath = "";
+            bool succesful = false;
+
+            while (!succesful)
+            {
+                var candidate = current.Parent;
+
+                if (candidate is null)
+                    throw new ArgumentException("No path found");
+
+
+                var fullCandidatePath = Path.Combine(candidate.FullName, path, suffix);
+                
+                if (Directory.Exists(fullCandidatePath))
+                {
+                    succesful = true;
+                    fullPath = fullCandidatePath;
+                }
+            }
+
+            return fullPath;
+        }
+    }
+}
